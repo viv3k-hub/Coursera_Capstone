843db21be9096756e99741c3acc53a87a78f12e3
viv3k-hub <53141622+viv3k-hub@users.noreply.github.com>
2019-07-21 18:05:50 +0530

    Create Capstone Project Course.py

---
+++ Capstone Project Course.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <h2> This notebook will be mainly used for the capstone project.<h2>
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[2]:
+
+
+import numpy as np
+
+
+# In[6]:
+
+
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
